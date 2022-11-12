e7b2b1b5591bc0dde1c0a2ebc32a608df90f5112
beeru405 <101712802+beeru405@users.noreply.github.com>
2022-11-12 17:21:02 +0530

    Update TestHelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/TestHelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/TestHelloWorld.java
@@ -1,7 +1,12 @@
-public void mainMethodTest() throws Exception{
-    ByteArrayOutputStream data = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(data, true, "UTF-8");
-    TestHelloWorld.doPrint(ps);
-    ps.flush();
-    Assert.assertEquals("Hello world") + System.getProperty("line.separator"), new String(data, "UTF-8"));
+import java.io.PrintStream;
+
+public class TestHelloWorld {
+
+    public final static void main(String[] args) {
+        doPrint(System.out);
+    }
+
+    static void doPrint(PrintStream ps) {
+        ps.println("Hello World");
+    }
 }
